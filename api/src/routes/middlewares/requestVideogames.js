ef4302ded63fd35dc5842ba473ec0a3a958bf76f
Your Name <you@example.com>
2021-07-21 00:10:32 -0300

    as

--- api/src/routes/middlewares/requestVideogames.js
+++ api/src/routes/middlewares/requestVideogames.js
@@ -3,28 +3,26 @@ const { Videogame, Genre } = require('../../db.js');
 
 
 const requestVideogame = async(req,res,next)=> {
-    //numero de videojuegos
-    let numOfGames = await Videogame.count();
-    // buscar por nombre de videojuego
-    console.log("la query es",req.query)
+    
+    //* buscar por nombre de videojuego
     let page = Number(req.query.page) || 1;
     const name = req.query.name;
     const genre = req.query.genre || null;
     const status = req.query.status || null;
     const order = req.query.order || null;
     const way = req.query.way || null;
-    //*page
-    //*name
-    //*genre
-    //*status
-    //*order
-    //*way
+    const limit = 15;
+    
+    // *Si se requiere por el nombre y en algun tipo de orden
     if(name && order && way){
         Videogame.findAndCountAll({
-            offset:(page - 1) * 15, 
-            limit: 15,
+            distinct: true,
+            offset:(page - 1) * limit, 
+            limit: limit,
             attributes: ['id','name', 'background_image', 'rating'],
-            include : Genre,
+            include : {
+                model: Genre,
+            },
             where: {
                 name:{
                     [Op.iLike]: `%${name}%`,
@@ -36,18 +34,24 @@ const requestVideogame = async(req,res,next)=> {
             result.rows.length === 0? res.status(422).send({details: "The video game dont exist",results: result.rows})
             : res.send({
                 count : result.count, 
+                pages: Math.ceil(result.count / limit),
                 page : page,
                 results : result.rows,
-                actualEndPoint : `http://localhost:3001/videogames?page=${page}&name=${name.replace(/ /g, "%20")}&order=${order}&way=${way}`,
+                actualEndPoint : `http://localhost:3001/videogames?page=${page}&name=${name.replace(/ /g, "%20").toLowerCase()}&order=${order}&way=${way}`,
             })
         })
         .catch(error => console.error(error))
+
+    //* Si la request solo es con el nombre
     }else if(name){
         Videogame.findAndCountAll({
-            offset:(page - 1) * 15, 
-            limit: 15,
+            distinct: true,
+            offset:(page - 1) * limit, 
+            limit: limit,
             attributes: ['id','name', 'background_image', 'rating'],
-            include : Genre,
+            include :{
+                model:Genre,
+            },
             where: {
                 name:{
                     [Op.iLike]: `%${name}%`,
@@ -58,16 +62,19 @@ const requestVideogame = async(req,res,next)=> {
             result.rows.length === 0? res.status(422).send({details: "The video game dont exist",results:result.rows})
             : res.send({
                 count : result.count, 
+                pages: Math.ceil(result.count / limit),
                 page : page,
                 results : result.rows,
-                actualEndPoint : `http://localhost:3001/videogames?page=${page}&name=${name.replace(/ /g, "%20")}`,
+                actualEndPoint : `http://localhost:3001/videogames?page=${page}&name=${name.replace(/ /g, "%20").toLowerCase()}`,
             })
         })
         .catch(error => console.error(error))
+
+    // *Si la request es sobre el genero con algun tipo de orden
     }else if(genre && order && way){
         Videogame.findAndCountAll({
-            offset:(page - 1) * 15, 
-            limit: 15,
+            offset:(page - 1) * limit, 
+            limit: limit,
             attributes: ['id','name', 'background_image', 'rating'],
             include:{
                 model: Genre,
@@ -83,16 +90,17 @@ const requestVideogame = async(req,res,next)=> {
         })
         .then(result => res.send({
             count : result.count,
+            pages: Math.ceil(result.count / limit ),
             page : page,
             results : result.rows,
             actualEndPoint : `http://localhost:3001/videogames?page=${page}&genre=${genre}&order=${order}&way=${way}`
         }))
         .catch(error => console.error(error));
-
+    //* Si la request es solo del gerero
     }else if(genre){
         Videogame.findAndCountAll({
-            offset:(page - 1) * 15, 
-            limit: 15,
+            offset:(page - 1) * limit, 
+            limit: limit,
             attributes: ['id','name', 'background_image', 'rating'],
             include:{
                 model: Genre,
@@ -106,15 +114,19 @@ const requestVideogame = async(req,res,next)=> {
         })
         .then(result => res.send({
             count : result.count,
+            pages: Math.ceil(result.count / limit ),
             page : page,
             results : result.rows,
             actualEndPoint : `http://localhost:3001/videogames?page=${page}&genre=${genre}`
         }))
         .catch(error => console.error(error));
+
+    //* Si la request por juegos de la plataforma / juegos creados en aulgun tipo de orden
     }else if(status && order && way){
         Videogame.findAndCountAll({
-            offset:(page - 1) * 15, 
-            limit: 15,
+            distinct: true,
+            offset:(page - 1) * limit, 
+            limit:limit,
             attributes: ['id','name', 'background_image', 'rating'],
             include :Genre,
             where:{
@@ -125,17 +137,20 @@ const requestVideogame = async(req,res,next)=> {
             order:[[order,way]]
         })
         .then(result => res.send({
-            count : result.count, 
+            count : result.count,
+            pages: Math.ceil(result.count / limit ), 
             page : page, 
             results : result.rows,
             actualEndPoint : `http://localhost:3001/videogames?page=${page}&status=${status.replace(/ /g, "%20").toLowerCase()}&order=${order}&way=${way}`
         }))
         .catch(error => console.error(error));
 
+    //* Si la request por juegos de la plataforma / juegos creados
     }else if(status){
         Videogame.findAndCountAll({
-            offset:(page - 1) * 15, 
-            limit: 15,
+            distinct:true,
+            offset:(page - 1) * limit, 
+            limit: limit,
             attributes: ['id','name', 'background_image', 'rating'],
             include :Genre,
             where:{
@@ -145,150 +160,50 @@ const requestVideogame = async(req,res,next)=> {
             }
         })
         .then(result => res.send({
-            count : result.count, 
+            count : result.count,
+            pages: Math.ceil(result.count / limit ), 
             page : page, 
             results : result.rows,
             actualEndPoint : `http://localhost:3001/videogames?page=${page}&status=${status.replace(/ /g, "%20").toLowerCase()}`
         }))
         .catch(error => console.error(error));
 
+    //* Si la request no tiene filtros y se requiere en algun orden
     }else if(order && way){
-        Videogame.findAll({
-            offset:(page - 1) * 15, 
-            limit: 15,
+        Videogame.findAndCountAll({
+            distinct: true,
+            offset:(page - 1) * limit, 
+            limit: limit,
             attributes: ['id','name', 'background_image', 'rating'],
             include :Genre,
             order:[[order,way]]
         })
         .then(result => res.send({
-            count : numOfGames,
+            count : result.count,
+            pages: Math.ceil(result.count / limit),
             page : page, 
-            results : result,
+            results : result.rows,
             actualEndPoint : `http://localhost:3001/videogames?page=${page}&order=${order}&way=${way}`
         }))
         .catch(error => console.error(error));
+    //* Si la request no tiene filtros
     }else{
-        Videogame.findAll({
-            offset:(page - 1) * 15, 
-            limit: 15,
+        Videogame.findAndCountAll({
+            distinct: true,
+            offset:(page - 1) * limit, 
+            limit: limit,
             attributes: ['id','name', 'background_image', 'rating'],
             include :Genre,
         })
         .then(result => res.send({
             count : numOfGames,
+            pages: Math.ceil(result.count / limit),
             page : page, 
-            results : result,
+            results : result.rows,
             actualEndPoint : `http://localhost:3001/videogames?page=${page}`
         }))
         .catch(error => console.error(error));
     }
-    
-    // if(req.query.hasOwnProperty("name")){
-    //     let { name } = req.query;
-    //     //let nameEndPoint = name.replace(" ","%20")
-    //     Videogame.findAndCountAll({
-    //         offset:(page - 1) * 20, 
-    //         limit: 20,
-    //         attributes: ['id','name', 'background_image', 'rating'],
-    //         include : Genre,
-    //         where: {
-    //             name:{
-    //                 [Op.iLike]: `%${name}%`,
-    //             }
-    //         }
-    //     })
-    //     .then(result => {
-    //         result.rows.length === 0? res.status(422).send({details: "The video game dont exist"})
-    //         : res.send({
-    //             count : result.count, 
-    //             page : page,
-    //             results : result.rows,
-    //             actualEndPoint : `http://localhost:3001/videogames?page=${page}&name=${name.replace(/ /g, "%20")}`,
-    //         })
-    //     })
-    //     .catch(error => console.error(error))
-
-    // //filtrar por genero    
-    // }else if(req.query.hasOwnProperty("genre")){
-    //     let { genre } = req.query;
-    //     Videogame.findAndCountAll({
-    //         offset:(page - 1) * 20, 
-    //         limit: 20,
-    //         attributes: ['id','name', 'background_image', 'rating'],
-    //         include:{
-    //             model: Genre,
-    //             where:{
-    //                 name:{
-    //                     [Op.iLike] : `%${genre}%`
-    //                 }
-                    
-    //             }
-    //         }
-    //     })
-    //     .then(result => res.send({
-    //         count : result.count,
-    //         page : page,
-    //         results : result.rows,
-    //         actualEndPoint : `http://localhost:3001/videogames?page=${page}&genre=${genre}`
-    //     }))
-    //     .catch(error => console.error(error));
-
-    // // filtar en forma asc o desc por nombre y por rating
-    // }else if(req.query.hasOwnProperty("order") && req.query.hasOwnProperty("way")){
-    //     let { order, way} = req.query;
-    //     Videogame.findAndCountAll({
-    //         offset:(page - 1) * 20, 
-    //         limit: 20,
-    //         attributes: ['id','name', 'background_image', 'rating'],
-    //         include: Genre,
-    //         order: [[order,way]]
-    //     })
-    //     .then(result => res.send({
-    //         count : result.count, 
-    //         page : page, 
-    //         results : result.rows,
-    //         actualEndPoint : `http://localhost:3001/videogames?page=${page}&order=${order}&way=${way}`
-    //     }))
-    //     .catch(error => console.error(error));
-
-    // // filtro por videojuego creado y no creado
-    // }else if(req.query.hasOwnProperty("status")){
-    //     let { status } = req.query;
-    //     Videogame.findAndCountAll({
-    //         offset:(page - 1) * 20, 
-    //         limit: 20,
-    //         attributes: ['id','name', 'background_image', 'rating'],
-    //         include :Genre,
-    //         where:{
-    //             status: status
-    //         }
-    //     })
-    //     .then(result => res.send({
-    //         count : result.count, 
-    //         page : page, 
-    //         results : result.rows,
-    //         actualEndPoint : `http://localhost:3001/videogames?page=${page}&status=${status}`
-    //     }))
-    //     .catch(error => console.error(error));
-        
-    // // buscar todos los video juegos de la base de datos
-    // }else{
-    //     Videogame.findAll({
-    //         offset:(page - 1) * 20, 
-    //         limit: 20,
-    //         attributes: ['id','name', 'background_image', 'rating'],
-    //         include :Genre,
-    //     })
-    //     .then(result => res.send({
-    //         count : numOfGames,
-    //         page : page, 
-    //         results : result,
-    //         actualEndPoint : `http://localhost:3001/videogames?page=${page}`
-    //     }))
-    //     .catch(error => console.error(error));
-        
-        
-    // }  
 }
 module.exports = {
     requestVideogame
